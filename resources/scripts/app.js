f8e54f033b2b00b8db614cb6ffe5df0208461754
hristo.s.hristov <hristo.s.hristov@mentormate.com>
2022-07-22 17:30:09 +0300

    added responsive design

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,8 +1,7 @@
-function codecademy () {
-    const link =
-      "<a href='https://github.com/hristo-hristov-bg/codecademy-projects'>https://github.com/hristo-hristov-bg/codecademy-projects</a>";
-    document.getElementById("codecademy").innerHTML =
-      link;
+function codecademy() {
+  const link =
+    "<a href='https://github.com/hristo-hristov-bg/codecademy-projects'>https://github.com/hristo-hristov-bg/codecademy-projects</a>";
+  document.getElementById("codecademy").innerHTML = link;
 }
 
 function typescript() {
@@ -21,4 +20,13 @@ function softuni() {
   const link =
     "<a href='https://github.com/hristo-hristov-bg/SoftUni-Courses'>https://github.com/hristo-hristov-bg/SoftUni-Courses</a>";
   document.getElementById("softuni").innerHTML = link;
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
